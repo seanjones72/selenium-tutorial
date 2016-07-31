3ab5ff911a0b3d78013bd9db62eff873ab47be9f
seanjones <nkirutech@gmail.com>
2016-07-31 02:08:24 -0400

    Initial commit

---
+++ src/test/java/com/mysite/selenium/SeleniumHQTest.java
@@ -0,0 +1,31 @@
+package com.mysite.selenium;
+
+import io.ddavison.conductor.Browser;
+import io.ddavison.conductor.Config;
+import io.ddavison.conductor.Locomotive;
+import org.junit.Test;
+
+@Config(
+        browser = Browser.CHROME,
+        url = "http://seleniumhq.org"
+)
+
+/**
+ * Created by macbook on 6/25/16.
+ */
+public class SeleniumHQTest extends Locomotive {
+    @Test
+    public void testDownloadLinkExist() {
+        validatePresent(HomePage.LOC_LNK_DOWNLOAD_SELENIUM);
+    }
+
+    @Test
+    public void testTabsExists() {
+        validatePresent(HomePage.LOC_LNK_PROJECTSTAB)
+                .validatePresent(HomePage.LOC_LNK_DOWNLOADTAB)
+                .validatePresent(HomePage.LOC_LNK_DOCUMENTATIONTAB)
+                .validatePresent(HomePage.LOC_LNK_SUPPORTAB)
+                .validatePresent(HomePage.LOC_LNK_ABOUTAB);
+
+    }
+}
